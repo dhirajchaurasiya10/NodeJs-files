44b9edcd962e84e071383611748422fdf8df0e45
dhirajchaurasiya10 <starkjon224@gmail.com>
2024-04-29 13:29:32 +0545

    added fs module

---
+++ fsmodule.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+//To read a file
+//readFile le yo function ko full content naauni ber smma read grxa ani tespxi ko command execute garirahanxa 
+// tyaivayera finished reading file is printed first rather than contents of readFile
+
+
+// fs.readFile('file.txt', 'utf8', (err, data)=> {
+//     console.log(err, data)
+// })
+
+
+//readfileSync blocks the program till the file is read
+const a = fs.readFileSync('file.txt')
+console.log(a);
+
+console.log("Finished reading file")
